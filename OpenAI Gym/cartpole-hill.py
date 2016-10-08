749871d040dd7e10a77a157b813121b8c7eae076
Yan JIN <rockking.jy@gmail.com>
2016-10-08 20:26:46 +0200

    Update cartpole-hill.py

--- OpenAI Gym/cartpole-hill.py
+++ OpenAI Gym/cartpole-hill.py
@@ -19,7 +19,7 @@ def run_episode(env, parameters):
 def train(submit):
     env = gym.make('CartPole-v0')
     if submit:
-        env.monitor.start('cartpole-hill/', force=True)
+        env.monitor.start('cartpole-experiments/', force=True)
 
     episodes_per_update = 5
     noise_scaling = 0.1
@@ -31,17 +31,19 @@ def train(submit):
         counter += 1
         newparams = parameters + (np.random.rand(4) * 2 - 1)*noise_scaling
         # print newparams
-        # reward = 0
-        # for _ in xrange(episodes_per_update):
-        #     run = run_episode(env,newparams)
-        #     reward += run
+        '''
+        reward = 0
+        for _ in xrange(episodes_per_update):
+            run = run_episode(env,newparams)
+            reward += run
+        '''
         reward = run_episode(env,newparams)
         # print "reward %d best %d" % (reward, bestreward)
         if reward > bestreward:
             # print "update"
             bestreward = reward
             parameters = newparams
-            if reward == 200:
+            if reward >= 200:
                 break
 
     if submit:
@@ -50,6 +52,5 @@ def train(submit):
         env.monitor.close()
     return counter
 
-
-r = train(submit=False)
+r = train(submit=True)
 print r
